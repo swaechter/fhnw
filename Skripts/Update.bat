2e032f1407de4c39afb98435b34adc97ab237e16
Simon Wächter <waechter.simon@gmail.com>
2019-10-03 18:00:39 +0200

    Add the initial documents of the new semester

---
+++ Skripts/Update.bat
@@ -0,0 +1,3 @@
+xcopy /E /Y /D "T:\E1811_Unterrichte_Bachelor\E1811_Unterrichte_I\5Iw\apsi" "Module\apsi"
+xcopy /E /Y /D "T:\E1811_Unterrichte_Bachelor\E1811_Unterrichte_I\5Is\ebssd" "Module\ebssd"
+xcopy /E /Y /D "T:\E1811_Unterrichte_Bachelor\E1811_Unterrichte_I\3Ia\cloud" "Module\cloud"
